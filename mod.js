5497b559388f59a15c5a21ec6476ceb16698f05a
Casper Beyer <caspervonb@pm.me>
2021-06-25 19:35:17 +0800

    Proxy raw requests to raw.githubusercontent.com

--- mod.js
+++ mod.js
@@ -1,8 +1,16 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(
-    new Response("Hello world", {
-      status: 200,
-      headers: { "content-type": "text/plain" },
-    }),
-  );
+addEventListener("fetch", async function(event) {
+  event.respondWith(await handleRequest(event.request));
 });
+
+async function handleRequest(request) {
+  const { pathname } = new URL(request.url);
+
+  if (pathname.endsWith(".json")) {
+    return fetch(`https://raw.githubusercontent.com/caspervonb/wasi-test-results/main/${pathname}`);
+  }
+
+  return new Response("Hello world", {
+    status: 200,
+    headers: { "content-type": "text/plain" },
+  });
+}
