a3a45c3429ce555eaa4140185296b4f3d311da6c
Casper Beyer <caspervonb@pm.me>
2021-06-26 02:17:44 +0800

    Add index handler

--- mod.js
+++ mod.js
@@ -5,12 +5,70 @@ addEventListener("fetch", async function(event) {
 async function handleRequest(request) {
   const { pathname } = new URL(request.url);
 
-  if (pathname.endsWith(".json")) {
-    return fetch(`https://raw.githubusercontent.com/caspervonb/wasi-test-results/main/${pathname}`);
+  if (pathname == "/") {
+    return handleIndex(request);
   }
 
-  return new Response("Hello world", {
-    status: 200,
-    headers: { "content-type": "text/plain" },
+  if (pathname.endsWith(".json")) {
+    return handleRaw(request);
+  }
+
+  return handleNotFound(request);
+}
+
+async function handleIndex(request) {
+  const reports = await Promise.all([
+    fetch(`https://raw.githubusercontent.com/caspervonb/wasi-test-results/main/wasmer.json`).then(x => x.json()),
+    fetch(`https://raw.githubusercontent.com/caspervonb/wasi-test-results/main/wasmtime.json`).then(x => x.json()),
+  ]);
+
+  const summarize = ({ runtime, results }) => {
+    const summary = {
+      total: results.length,
+      passed: results.filter(x => x.status == "PASS").length,
+    };
+
+    return `
+      <section>
+        <p>Ran ${summary.total} tests with ${runtime.name} (v${runtime.version})</p>
+        <p>${summary.passed} / ${summary.total} test cases pass.</p>
+        <progress value="${summary.passed}" max="${summary.total}">
+      </section>
+    `;
+  };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        ${reports.map(summarize)}
+      </body>
+    </html>
+  `;
+
+  return new Response(html, {
+    status: 404,
+    headers: { "content-type": "text/html" },
+  });
+}
+
+async function handleRaw(request) {
+  const { pathname } = new URL(request.url);
+  return fetch(`https://raw.githubusercontent.com/caspervonb/wasi-test-results/main/${pathname}`);
+}
+
+async function handleNotFound(request) {
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <h2>Not Found</h2>
+      </body>
+    </html>
+  `;
+
+  return new Response(html, {
+    status: 404,
+    headers: { "content-type": "text/html" },
   });
 }
